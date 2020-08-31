773b2d9e3cb5532ea59b51ebc092ef0f86b93da3
Raghid Alhazmy <raghid.alhazmy@gmail.com>
2020-08-31 09:47:42 -0400

    Added lab1 assignment

---
+++ lab1_RFA.R
@@ -0,0 +1,21 @@
+##Exercise 1
+x<-3
+y<-15
+y + x
+y * x
+y - x
+y / x
+
+##Exercise 2
+x <- 2
+y <- 3
+x^5 > y^4
+
+##Exercise 3
+vctr<-c(22,62,148,43,129)
+sum(vctr)
+vctr /  sum(vctr)
+
+##Exercise 4
+nucleotide<-c("A","T","C","G")
+sort(nucleotide)
